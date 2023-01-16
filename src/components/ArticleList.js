0c394e654deecb664c356f7ed6e36bf709a14c0d
maqiie <99504010+maqiie@users.noreply.github.com>
2023-01-16 08:57:51 +0300

    completed the lab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({id}) {
+    return (
+        <main>
+            <Article key={id}/>
+            <Article key={id}/>
+            <Article key={id}/>
+        </main>
+    )
+}
+
+export default ArticleList;
